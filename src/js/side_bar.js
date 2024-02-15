9f22bf209a8cf483a7d16881189bdf044e8eeff1
Praveen8760 <praveen890340@gmail.com>
2024-02-15 09:35:25 +0530

    nav_side_fixed

--- src/js/side_bar.js
+++ src/js/side_bar.js
@@ -1,4 +1,4 @@
-const icon_node=document.getElementById('nav_side')
+const icon_node=document.getElementById('nav_side_icon')
 const body_node=document.getElementById('body')
 const navbar_node=document.getElementById('nav')
 const nav_icon=document.getElementById('nav_icon')
